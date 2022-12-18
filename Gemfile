theforeman/smart_proxy_monitoring

commit a45571e0e14acf6a074fe4c326d9818ed98abbcc

Make compatible with the Jenkins testing pipeline

Gemfile
@@ -1,15 +1,18 @@
 source 'https://rubygems.org'
 gemspec
 
-group :development do
+group :rubocop do
   gem 'rubocop', '~> 1.28.0'
   gem 'rubocop-performance'
   gem 'rubocop-rake'
 end
 
 group :test do
+  gem 'mocha'
   gem 'public_suffix'
   gem 'rack-test'
-  gem 'smart_proxy', git: 'https://github.com/theforeman/smart-proxy.git', branch: 'develop'
+  gem 'rake'
+  gem 'smart_proxy', github: 'theforeman/smart-proxy', branch: 'develop'
+  gem 'test-unit'
   gem 'webmock'
 end

smart_proxy_monitoring.gemspec
@@ -16,8 +16,4 @@ Gem::Specification.new do |s|
   s.add_dependency 'rest-client'
 
   s.required_ruby_version = '>= 2.5', '< 3'
-
-  s.add_development_dependency('mocha')
-  s.add_development_dependency('rake')
-  s.add_development_dependency('test-unit')
 end

commit 5a60321cfc4be5b7a2b1d9aa39b0dbc55295c177

Complete the Jenkins CI pipeline migration
Fixes: a45571e0e14acf6a074fe4c326d9818ed98abbcc

Gemfile
@@ -8,6 +8,7 @@ group :rubocop do
 end
 
 group :test do
+  gem 'ci_reporter_test_unit'
   gem 'mocha'
   gem 'public_suffix'
   gem 'rack-test'

Rakefile
@@ -1,8 +1,12 @@
+require 'ci/reporter/rake/test_unit'
 require 'rake'
 require 'rake/testtask'
+require 'rubocop/rake_task'
+
+RuboCop::RakeTask.new
 
 desc 'Default: run unit tests.'
-task default: :test
+task :default => %i[rubocop test]
 
 desc 'Test the Foreman Proxy plugin.'
 Rake::TestTask.new(:test) do |t|
@@ -11,4 +15,9 @@ Rake::TestTask.new(:test) do |t|
   t.libs << 'test'
   t.test_files = FileList['test/**/*_test.rb']
   t.verbose = true
+end
+
+namespace :jenkins do
+  desc nil # No description means it's not listed in rake -T
+  task :unit => ['ci:setup:testunit', :test]
 end