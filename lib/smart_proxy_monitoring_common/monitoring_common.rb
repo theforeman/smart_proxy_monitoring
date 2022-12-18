1e11f0132af160f4866a9d7bdd7f9fcd4fd8dcdf
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-12-18 15:22:38 +0100

    Make Provider an abstract class with method signatures

--- lib/smart_proxy_monitoring_common/monitoring_common.rb
+++ lib/smart_proxy_monitoring_common/monitoring_common.rb
@@ -3,5 +3,29 @@ module Proxy::Monitoring
   class NotFound < RuntimeError; end
   class AuthenticationError < RuntimeError; end
 
-  class Provider; end
+  class Provider
+    def query_host(host)
+      raise NotImplementedError
+    end
+
+    def create_host(host, attributes)
+      raise NotImplementedError
+    end
+
+    def update_host(host, attributes)
+      raise NotImplementedError
+    end
+
+    def remove_host(host)
+      raise NotImplementedError
+    end
+
+    def remove_downtime_host(host, author, comment)
+      raise NotImplementedError
+    end
+
+    def set_downtime_host(host, author, comment, start_time, end_time, all_services: nil, **)
+      raise NotImplementedError
+    end
+  end
 end
